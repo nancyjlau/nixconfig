da94ff52a702e87a5cb05048fba2073d59b66f00
Nancy Lau <nancyjlau@gmail.com>
2024-11-23 21:35:45 -0800

    Update configuration and move to git

--- configuration.nix
+++ configuration.nix
@@ -1,30 +1,70 @@
 { config, pkgs, ... }:
-
 {
-  imports = [ <home-manager/nix-darwin> ];
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
-  environment.systemPackages = with pkgs;[
-      neovim
-      curl
-      git
-      python3
-      file
-      virtualenv
-    ];
+  imports = [ 
+    <home-manager/nix-darwin>
+  ];
+
+  # Your existing system packages
+  environment.systemPackages = with pkgs; [
+    neovim
+    curl
+    git
+    python3
+    file
+    virtualenv
+    python312Packages.cmake
+  ];
+
+  # Make sure shell paths include the user profile
+  environment.profiles = [
+    "/etc/profiles/per-user/rose"
+  ];
+
+  users.users.rose = {
+    name = "rose";
+    home = "/Users/rose";
+  };
+
+  # Home Manager configuration
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users.rose = { pkgs, ... }: {
+      home = {
+        enableNixpkgsReleaseCheck = false;
+        username = "rose";
+        homeDirectory = "/Users/rose";
+        # Set session path to include the user profile
+        sessionPath = [ "/etc/profiles/per-user/rose/bin" ];
+        packages = with pkgs; [
+          atool
+          httpie
+          binwalk
+          pwntools
+          nushell
+          tmux
+        ];
+        stateVersion = "24.05";
+      };
+      
+      programs.home-manager.enable = true;
+      
+      # Configure zsh to load the environment
+      programs.zsh = {
+        enable = true;
+        initExtra = ''
+          export PATH="/etc/profiles/per-user/rose/bin:$PATH"
+          if [ -e '/etc/profiles/per-user/rose/etc/profile.d/hm-session-vars.sh' ]; then
+            . '/etc/profiles/per-user/rose/etc/profile.d/hm-session-vars.sh'
+          fi
+        '';
+      };
+    };
+  };
 
   services.nix-daemon.enable = true;
   nix.package = pkgs.nix;
-
-  # Use custom location for configuration.nix.
   environment.darwinConfig = "$HOME/.config/nix-darwin/configuration.nix";
-
-  # Enable alternative shell support in nix-darwin.
-  # programs.nushell.enable = true;
-  environment.shells = with pkgs; [                                                nushell
-  ];
-
-  # Used for backwards compatibility, please read the changelog before changing.
-  # $ darwin-rebuild changelog
+  environment.shells = with pkgs; [ nushell ];
   system.stateVersion = 5;
 }
