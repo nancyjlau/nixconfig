05d9f710f25426102ed8f88e62fb59b2dd285500
Nancy Lau <nancyjlau@gmail.com>
2024-12-25 19:59:36 -0800

    neovim config

--- configuration.nix
+++ configuration.nix
@@ -1,11 +1,12 @@
 { config, pkgs, ... }:
 {
-  imports = [ 
+  imports = [
     <home-manager/nix-darwin>
   ];
 
   environment.systemPackages = with pkgs; [
     neovim
+    nil
     curl
     git
     python3
@@ -18,13 +19,12 @@
     poetry
   ];
 
-    homebrew = {
+  homebrew = {
     enable = true;
     # onActivation.cleanup = "uninstall";
-
     taps = [];
-    brews = [ "libiconv" "git-lfs"]; 
-    casks = [];
+    brews = [ "libiconv" "git-lfs" "gradle" "aria2" ];
+    casks = [ "ghidra" ];
   };
 
   environment.profiles = [
@@ -48,20 +48,22 @@
         # Set session path to include the user profile
         sessionPath = [ "/etc/profiles/per-user/rose/bin" ];
         packages = with pkgs; [
-	  ocaml
-	  ihaskell
+          ocaml
+          ihaskell
           atool
           httpie
           binwalk
           pwntools
           nushell
           tmux
+          python312Packages.matplotlib
+          openjdk
         ];
         stateVersion = "24.05";
       };
-      
+
       programs.home-manager.enable = true;
-      
+
       # Configure zsh to load the environment
       programs.zsh = {
         enable = true;
@@ -88,6 +90,100 @@ EOF
           if [ -f "$HOME/.cargo/env" ]; then
              . "$HOME/.cargo/env"
           fi
+        '';
+      };
+
+      # Neovim configuration
+      programs.neovim = {
+        enable = true;
+        defaultEditor = true;
+        viAlias = true;
+        vimAlias = true;
+
+        plugins = with pkgs.vimPlugins; [
+          {
+            plugin = tokyonight-nvim;
+            config = "colorscheme tokyonight-night";
+          }
+          nvim-treesitter.withAllGrammars
+          telescope-nvim
+          nvim-tree-lua
+          lualine-nvim
+          nvim-lspconfig
+          nvim-cmp
+          cmp-nvim-lsp
+          cmp-buffer
+          cmp-path
+          luasnip
+          gitsigns-nvim
+        ];
+
+        extraConfig = ''
+          set number
+          set relativenumber
+          set shiftwidth=2
+          set tabstop=2
+          set expandtab
+          set smartindent
+          set nowrap
+          set noswapfile
+          set nobackup
+          set undofile
+          set nohlsearch
+          set termguicolors
+          set scrolloff=8
+          set updatetime=50
+
+          " Set leader key to space
+          let mapleader = " "
+
+          " Key mappings
+          nnoremap <leader>e :NvimTreeToggle<CR>
+          nnoremap <leader>ff <cmd>Telescope find_files<cr>
+          nnoremap <leader>fg <cmd>Telescope live_grep<cr>
+          nnoremap <leader>fb <cmd>Telescope buffers<cr>
+          nnoremap <leader>h :bprevious<CR>
+          nnoremap <leader>l :bnext<CR>
+          nnoremap <leader>w :w<CR>
+
+          " Plugin configurations
+          lua << EOF
+          -- LSP configurations
+          require('lspconfig').pyright.setup{}
+          require('lspconfig').ocamllsp.setup{}
+          require('lspconfig').hls.setup{}
+          require('lspconfig').nil_ls.setup{}
+
+          -- Treesitter configuration
+          require('nvim-treesitter.configs').setup {
+            highlight = { enable = true },
+          }
+
+          -- Completion setup
+          local cmp = require'cmp'
+          cmp.setup({
+            snippet = {
+              expand = function(args)
+                require('luasnip').lsp_expand(args.body)
+              end,
+            },
+            mapping = cmp.mapping.preset.insert(),
+            sources = {
+              { name = 'nvim_lsp' },
+              { name = 'buffer' },
+              { name = 'path' },
+              { name = 'luasnip' },
+            }
+          })
+
+          -- Lualine setup
+          require('lualine').setup {
+            options = { theme = 'tokyonight' }
+          }
+
+          -- Gitsigns setup
+          require('gitsigns').setup()
+          EOF
         '';
       };
     };
